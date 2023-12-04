aaea883413851d167c2198eef656baeb21adac24
dimapartov <mr_kubarev@mail.ru>
2023-12-04 12:24:42 +0300

    Add custom method usage

--- src/main/java/trees/bst/MainBinarySearchTree.java
+++ src/main/java/trees/bst/MainBinarySearchTree.java
@@ -1,5 +1,8 @@
 package trees.bst;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class MainBinarySearchTree {
     public static void main(String[] args) {
 
@@ -36,5 +39,12 @@ public class MainBinarySearchTree {
 
         AbstractBinarySearchTree.Node<Integer> right = binarySearchTree.getRight();
         System.out.println("Right: " + right.value);
+
+        System.out.println("------------------------------------Find duplicates-----------------------------------");
+        BinarySearchTree<Integer> bst = new BinarySearchTree<>();
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 2, 1, 4, 5, 6, 7, 8, 9, 9, 10);
+        List<Integer> duplicates = bst.findDuplicates(numbers);
+        System.out.println("Duplicates: " + duplicates);
+        
     }
 }
